4df7b11808f2323b72d8383a9a2153a80538aac6
Grey <sergejyurskyj@yahoo.com>
2020-06-27 13:51:22 -0700

    OpenGL_Graphics_Programming video 3

--- Video2_Setting_Up_Your_Environment/src/lesson1.cpp
+++ Video2_Setting_Up_Your_Environment/src/lesson1.cpp
@@ -4,8 +4,59 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
+// Global Variables
+const char* APP_TITLE = "Introduction to Modern OpenGL - Hello Window 1";
+const int gWindowWidth = 800;
+const int gWindowHeight = 600;
+
+//-----------------------------------------------------------------------------
+// Main Application Entry Point
+//-----------------------------------------------------------------------------
+
 int main()
 {
-	
+	if (!glfwInit())
+	{
+		std::cerr << "GLFW  initialization failed" << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+
+		if (pWindow == NULL)
+		{
+			std::cerr << "Failed to create GLFW window" << std::endl;
+			glfwTerminate();
+			return -1;
+		}
+
+	glfwMakeContextCurrent(pWindow);
+
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	// Rendering loop
+	while (!glfwWindowShouldClose(pWindow))
+	{
+		glfwPollEvents();
+
+		glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(pWindow);
+	}
+
+	// Clean up
+	glfwTerminate();
 	return 0;
 }
